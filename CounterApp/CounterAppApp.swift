466b01faedabb43fae715f8ff1b01542dc3f26e0
minesyouto <minesyouto8833@gmail.com>
2022-01-30 23:17:02 +0900

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by みねた on 2022/01/30.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
