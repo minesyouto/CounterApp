b972ed971c0ef4d8384d1ed218c45a920bd86bea
minesyouto <minesyouto8833@gmail.com>
2022-01-31 11:27:49 +0900

    CounterApp:fin

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -5,17 +5,29 @@
 //  Created by みねた on 2022/01/30.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
-    @State var number = 0
+    @State var number = 0 //@Stateというプロパティラッパーを置くとそのプロパティの値が更新されたときに自動的にiPhoneの画面が更新される→Ajaxに近いイメージ？
     var body: some View {
-        VStack {
+        VStack { //VStackを使って垂直方向にUI部品を並べている
+            ZStack{ //画像と文字を重ねている
+            Image("counter")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
             Text("\(number)")
-            Button(action: {self.number += 1}) { //この部分がボタンのアクション内容
-                Text("カウント") //この部分がボタンの中身
-                //ボタンは引数が値ではなく文になる
+                .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
             }
+            Button(action: {self.number += 1}) {
+                Text("カウント")
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10.0)
+            }
+
         }
     }
 }
@@ -25,3 +37,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
